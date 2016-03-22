9b0059dc7cbc7360eec4a9c28a47eb5fc3a5a61b
shuntaroy <shunt.yada@gmail.com>
2016-03-22 17:44:05 +0900

    improve navbar UX by scrolltop/scrollspy

--- custom.js
+++ custom.js
@@ -1,11 +1,10 @@
-// jQuery(function() {
-//   'use strict';
-//   jQuery('footer a').smoothScroll({speed: 400});
-// });
-
 $('#navbar a').smoothScroll();
-$('a.navbar-brand').smoothScroll();
+$("#brand").on('click', function(){
+    $("body,html").animate({scrollTop:0}, 'slow');
+    return false;
+});
 $('.nav a').on('click', function(){
     $('.btn-navbar').click(); //bootstrap 2.x
     $('.navbar-toggle').click() //bootstrap 3.x by Richard
+    $('body').scrollspy({ target: '#navbar' });
 });
