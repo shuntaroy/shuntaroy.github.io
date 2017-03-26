674d9b1ae525d20f2cb2bf61d364230bf1ce25cc
shuntaroy <shunt.yada@gmail.com>
2017-03-26 11:16:38 +0900

    auto beautify

--- custom.js
+++ custom.js
@@ -1,10 +1,14 @@
 $('#navbar a').smoothScroll();
-$("#brand").on('click', function(){
-    $("body,html").animate({scrollTop:0}, 'slow');
+$("#brand").on('click', function() {
+    $("body,html").animate({
+        scrollTop: 0
+    }, 'slow');
     return false;
 });
-$('.nav a').on('click', function(){
+$('.nav a').on('click', function() {
     $('.btn-navbar').click(); //bootstrap 2.x
     $('.navbar-toggle').click() //bootstrap 3.x by Richard
-    $('body').scrollspy({ target: '#navbar' });
+    $('body').scrollspy({
+        target: '#navbar'
+    });
 });
