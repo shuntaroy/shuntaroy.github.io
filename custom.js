6e40cbecfe6c50c556810d14a9ab3798746b3511
shuntaroy <shuntaroy@p.u-tokyo.ac.jp>
2017-07-30 18:10:17 +0900

    sample state of multi languages (experimental)

--- custom.js
+++ custom.js
@@ -1,10 +1,12 @@
 $('#navbar a').smoothScroll();
+
 $("#brand").on('click', function() {
     $("body,html").animate({
         scrollTop: 0
     }, 'slow');
     return false;
 });
+
 $('.nav a').on('click', function() {
     $('.btn-navbar').click(); //bootstrap 2.x
     $('.navbar-toggle').click() //bootstrap 3.x by Richard
