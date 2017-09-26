b1b63d12b34514169d9f37718ec9bfbc4ab952e7
shuntaroy <shuntaroy@p.u-tokyo.ac.jp>
2017-09-26 13:42:37 +0900

    add Japanese version
    close #2

--- custom.js
+++ custom.js
@@ -1,16 +1,8 @@
-$('#navbar a').smoothScroll();
 
-$("#brand").on('click', function() {
-    $("body,html").animate({
-        scrollTop: 0
-    }, 'slow');
-    return false;
-});
-
-$('.nav a').on('click', function() {
-    $('.btn-navbar').click(); //bootstrap 2.x
-    $('.navbar-toggle').click() //bootstrap 3.x by Richard
-    $('body').scrollspy({
-        target: '#navbar'
-    });
-});
+// $('.nav a:not(.lang)').on('click', function() {
+//     $('.btn-navbar').click(); //bootstrap 2.x
+//     $('.navbar-toggle').click() //bootstrap 3.x by Richard
+//     $('body').scrollspy({
+//         target: '#navbar'
+//     });
+// });
