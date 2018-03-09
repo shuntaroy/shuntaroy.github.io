21207fdfda51e02cbf2920606e6f08b093b5a275
shuntaroy <shuntaroy@p.u-tokyo.ac.jp>
2018-03-09 23:39:06 +1100

    modify js

--- custom.js
+++ custom.js
@@ -6,3 +6,35 @@
 //         target: '#navbar'
 //     });
 // });
+
+// smoothScroll
+$('#navbar a:not(.lang)').smoothScroll();
+$("#brand").on('click', function() {
+  $("body,html").animate({
+    scrollTop: 0
+  }, 'slow');
+  return false;
+});
+$('.navbar-collapse a').click(function() {
+  $(".navbar-collapse").collapse('hide');
+});
+
+// multi-lang
+$(document).ready(function() {
+  var setLanguage = function(lang) {
+    lang = lang.substr(0, 2);
+    var from = lang == 'en' ? 'ja' : 'en';
+    $('.lang-' + from).hide();
+    $('.lang-' + lang).fadeIn();
+    $('.lang').parent().removeClass("active")
+    $('.lang').each(function(i, el) {
+      if ($(el).data("lang") == lang) {
+        $(el).parent().addClass("active");
+      }
+    });
+  };
+  $(".lang").click(function() {
+    setLanguage($(this).data("lang"));
+  });
+  setLanguage(navigator.language || navigator.userLanguage || "en");
+});
