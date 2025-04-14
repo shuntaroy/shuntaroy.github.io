652fa54eeccc96b4d6b79c291ff4f5fa21a54e66
Shuntaro Yada <contact@shuntaroy.com>
2025-04-15 00:10:56 +0900

    split pages into each language

--- custom.js
+++ custom.js
@@ -23,24 +23,6 @@ $(document).ready(function () {
     });
     $('#down a').smoothScroll({ offset: -50 });
 
-    // Switch language (default: English)
-    var setLanguage = function (lang) {
-        lang = lang.substr(0, 2);
-        var from = lang == 'en' ? 'ja' : 'en';
-        $('.lang-' + from).hide();
-        $('.lang-' + lang).fadeIn();
-        $('.lang').parent().removeClass("active")
-        $('.lang').each(function (i, el) {
-            if ($(el).data("lang") == lang) {
-                $(el).parent().addClass("active");
-            }
-        });
-    };
-    $(".lang").click(function () {
-        setLanguage($(this).data("lang"));
-    });
-    setLanguage(navigator.language || navigator.userLanguage || "en");
-
     // A work-around until Line Awesome supports Notion icon
     $("#notion_icon").hover(function () {
         $("#notion_icon").attr('src', "./assets/notion-logo-no-background.png");
